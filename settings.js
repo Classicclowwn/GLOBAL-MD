ea0b0a52a4f38926cd362831bfe611171cd0f362
Classicclowwn <114720751+Classicclowwn@users.noreply.github.com>
2024-09-25 23:02:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,24 +4,24 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //owmner v card
-global.ytname = "YT: GlobalTechInfo" //ur yt chanel name
-global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
-global.location = "Pakistan, Karachi, Karachi" //ur location
+global.ytname = "null" //ur yt chanel name
+global.socialm = "GitHub: Classicclowwn" //ur github or insta name
+global.location = "Nigeria" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
-global.websitex = "https://www.youtube.com/@GlobalTechInfo"
-global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
+global.botname = '© CLOWN-MD' //ur bot name
+global.ownernumber = ['2348163243259'] //ur owner number, dont add more than one
+global.ownername = '© Classicclown' //ur owner name
+global.websitex = "null"
+global.wagc = "null"
 global.themeemoji = '🪀'
 global.wm = "Global Bot Inc."
-global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
-global.packname = "Sticker By"
+global.botscript = 'https://github.com/Classicclowwn/GLOBAL-MD.git' //script link
+global.packname = "Sticker By classicclowwn"
 global.author = "GlobalTechInfo\n\n+923444844060"
 global.creator = "923444844060@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["2348163243259"] // Premium User
 
 //channel id
 global.xchannel = {
